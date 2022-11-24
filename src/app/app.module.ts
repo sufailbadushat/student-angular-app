afbf10b767abc578e5beb2387be3eb3febb8ca4b
sufailbadushat <sufailbadushat@gamil.com>
2022-11-24 10:30:38 +0530

    student entry added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StudentEntryComponent } from './student-entry/student-entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentEntryComponent
   ],
   imports: [
     BrowserModule
